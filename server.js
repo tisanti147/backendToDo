66f3a5a6ed57e15bd411935dbb1a38b374b5fa16
tisanti147 <santid147@gmail.com>
2024-03-12 18:13:59 -0300

    clg msg

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use('/tasks', tasksRouter);
 app.use('/users', usersRouter);
 
 app.listen(10000, () => {
-    console.log('Server running on port 3000');
+    console.log('Server running on port 10000');
 });
